5bda8dd39daf0130185244e87dc89be9844190b8
timvisee <timvisee@gmail.com>
2018-03-24 22:01:58 +0100

    Update Cargo.lock, change version number and about info

--- cli/src/app.rs
+++ cli/src/app.rs
@@ -2,10 +2,10 @@
 pub const APP_NAME: &'static str = "ffsend";
 
 /// The application version.
-pub const APP_VERSION: &'static str = "0.1";
+pub const APP_VERSION: &'static str = "0.1-dev";
 
 /// The application author.
-pub const APP_AUTHOR: &'static str = "Tim Visee <timvisee@gmail.com>";
+pub const APP_AUTHOR: &'static str = "Tim Visee <https://timvisee.com/>";
 
 /// Application about information.
 pub const APP_ABOUT: &'static str = "A simple Firefox Send CLI client.";
